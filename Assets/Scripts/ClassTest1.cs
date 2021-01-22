31eed098e0f0a65e5c3c55d459c36a7f894f1c41
Juan Franco <juanferfranco@gmail.com>
2021-01-22 11:04:51 -0500

    Add code from feature 1

---
+++ Assets/Scripts/ClassTest1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassTest1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("code from feature 1");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
